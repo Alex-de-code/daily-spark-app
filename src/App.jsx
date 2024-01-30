7a464e724a7fc7ba0ea0a1a3615512c0c0da0112
jadenglenn <jadenglenn@pursuit.org>
2024-01-30 16:00:50 -0500

    initial commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
+
+import Home from "./Landing";
+import Quotes from "./Quotes";
 import "./App.css";
 
 function App() {
   return (
     <>
+    <Home/>
       <div>Hello</div>
     </>
   );
---
+++ src/Quotes.jsx
@@ -0,0 +1,9 @@
+function Quotes() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Quotes
