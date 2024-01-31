5411f9531e0457ef2b6f721878da408dc54804ff
jadenglenn <jadenglenn@pursuit.org>
2024-01-31 16:56:29 -0500

    retweak to nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -7,9 +7,9 @@ const Nav = () => {
   return (
     <>
   <div className="flex-wrap justify-evenly items-center bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-indigo-200 via-red-200 to-yellow-100">
-    <h2 className="text-2xl text-shadow cursor-pointer"><Link to="/">All Sparks !</Link></h2>
+    <h2 className="text-2xl text-shadow cursor-pointer"><Link to="/">Daily Sparks !</Link></h2>
     <ul className="flex justify-between">
-      <li className="flex-1 text-center hover:text-green-100 text-shadow-sm cursor-pointer drop-shadow-md scale-90"><Link to="src/SavedQuotes"> Your Sparks</Link></li>
+      <li className="flex-1 text-center hover:text-green-100 text-shadow-sm cursor-pointer drop-shadow-md scale-90"><Link to="src/SavedQuotes">Sparks</Link></li>
       <li className="flex-1 text-center hover:text-green-100 text-shadow-sm cursor-pointer drop-shadow-md scale-90"><Link to="CreateQuote">Make a Spark</Link></li>
       <li className="flex-1 text-center hover:text-green-100 text-shadow-sm cursor-pointer drop-shadow-md scale-90"><Link to="src/Favorite">Favorite Sparks</Link> </li>
     </ul>
