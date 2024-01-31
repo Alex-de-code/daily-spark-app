87c0379cea6ed37b89bed7e43ddeecbfecd3bd40
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-01-31 12:39:40 -0500

    fixed dataflow

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,22 @@ import { Route, Routes } from "react-router-dom";
 import Landing from "./Components/Landing";
 import Nav from "./Components/Nav";
 import Footer from "./Components/Footer";
+import { getAllData } from "../data/fetch";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [allQuotes, setAllQuotes] = useState([]); //state of Quotes
+  const [allBackgrounds, setAllBackgrounds] = useState([]); //state of Backgrounds
+
+  useEffect(() => {
+    setAllQuotes(getAllData("quotes", setAllQuotes, setAllBackgrounds)); // should get quotes data
+    setAllBackgrounds(
+      getAllData("backgrounds", setAllQuotes, setAllBackgrounds)
+    ); // should get backgrounds data
+  }, []);
+
+  console.log(allQuotes);
+
   return (
     <>
       <Nav />
