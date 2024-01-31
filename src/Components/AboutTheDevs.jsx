b930d548c0d3ad9f482a2d42835770e38177c718
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-01-31 15:57:07 -0500

    added shadows and animations to about the dev page to make it look more dynamic and appealing. Also added proper icons

--- src/Components/AboutTheDevs.jsx
+++ src/Components/AboutTheDevs.jsx
@@ -4,21 +4,20 @@ import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 import { BsGithub } from "react-icons/bs";
 import { FaLinkedin } from "react-icons/fa";
 import { MdAlternateEmail } from "react-icons/md";
+import { FaLaptopCode } from "react-icons/fa6";
+import { BsGearWide } from "react-icons/bs";
+import { MdOutlineDesignServices } from "react-icons/md";
 
 export const AboutTheDevs = () => {
   return (
     <>
       <div className="">
-        <h1 className="p-10 text-3xl text-center font-semibold bg-zinc-100">
+        <h1 className="p-10 text-3xl text-center font-semibold bg-zinc-100 shadow-lg">
           About the Developers
         </h1>
         <div className="p-5 flex flex-wrap justify-between bg-gradient-to-t from-blue-500 via-sky-400 to-cyan-600">
-          <div className="mx-2 my-2 p-5 flex-1 flex flex-col items-center rounded-lg bg-white bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-90 border border-gray-100">
-            <img
-              src="src/Assets/140419156.png"
-              alt="github avatar"
-              className="rounded-full p-20 w-15 h-auto object-center"
-            />
+          <div className="mx-2 my-2 p-5 flex-1 flex flex-col items-center rounded-lg bg-white shadow-2xl backdrop-blur-3xl bg-opacity-90 border border-gray-100">
+            <FaLaptopCode className="rounded-full my-6 p-5 w-40 h-auto object-center bg-yellow-400 transition duration-700 delay-700 hover:animate-bounce shadow-lg" />
             <div className="m-4">
               Jaden Glenn is a Software Engineer with a passion for technology
               and a keen interest in cybersecurity.
@@ -44,12 +43,8 @@ export const AboutTheDevs = () => {
               </Link>
             </div>
           </div>
-          <div className=" mx-2 my-2 p-5 flex-1 flex flex-col items-center rounded-lg bg-white bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-90 border border-gray-100">
-            <img
-              src="src/Assets/144467503.png"
-              alt="github avatar"
-              className="rounded-full p-20 w-15 h-auto object-center"
-            />
+          <div className=" mx-2 my-2 p-5 flex-1 flex flex-col items-center rounded-lg bg-white shadow-2xl backdrop-blur-3xl bg-opacity-90 border border-gray-100">
+            <BsGearWide className="rounded-full my-6 p-5 w-40 h-auto object-center bg-yellow-400 transition duration-700 delay-700 hover:animate-bounce shadow-lg" />
             <div className="m-4">
               Carlitos Dutan is a Software Engineer passionate about front-end
               development, problem-solving, and collaborating with a team.
@@ -74,12 +69,8 @@ export const AboutTheDevs = () => {
               </Link>
             </div>
           </div>
-          <div className=" mx-2 my-2 p-5 flex-1 flex flex-col items-center rounded-lg bg-white bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-90 border border-gray-100">
-            <img
-              src="src/Assets/140418415.png"
-              alt="github avatar"
-              className="rounded-full p-20 w-15 h-auto object-center"
-            />
+          <div className=" mx-2 my-2 p-5 flex-1 flex flex-col items-center rounded-lg bg-white shadow-2xl backdrop-blur-3xl bg-opacity-90 border border-gray-100">
+            <MdOutlineDesignServices className="rounded-full my-6 p-5 w-40 h-auto bg-yellow-400 transition duration-700 delay-700 hover:animate-bounce shadow-lg" />
             <div className="m-4">
               Alexander Gonzalez is a Software Engineer driven by a relentless
               pursuit of crafting timeless user experiences through creative
