a78bf4e7a268efad00d0c7f1d1ae56512179f041
Carlitos <carlitosdutan@pursuit.org>
2024-01-31 11:42:10 -0500

    files created

---
+++ data/fetch.js
@@ -0,0 +1,8 @@
+const URL = import.meta.env.VITE_BASE_API_URL;
+
+// export function getAllQuotes() {
+//   return fetch(`${URL}`).then((response) => {
+//     console.log(response);
+//     response.json();
+//   });
+// }
