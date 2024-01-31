2c3141ac30606aa93a3b7a4fa92bf527a99fad02
jadenglenn <jadenglenn@pursuit.org>
2024-01-31 11:47:18 -0500

    start to Nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,5 +1,16 @@
 const Nav = () => {
-  return <div className="bg-red-400">Nav</div>;
+  return (
+    <>
+  <div className="flex-wrap justify-evenly items-center bg-[conic-gradient(var(--tw-gradient-stops))] from-indigo-200 via-red-200 to-yellow-100">
+    <h2 className="text-2xl">Daily Spark !</h2>
+    <ul className="flex justify-between">
+      <li className="flex-1 text-center">Your Sparks</li>
+      <li className="flex-1 text-center">Make a Spark</li>
+      <li className="flex-1 text-center">Favorite Sparks</li>
+    </ul>
+  </div>
+  </>
+)
 };
 
 export default Nav;
