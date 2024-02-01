4b0a3b2970b810bfaebe16fd3865c9ef646301d1
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-02-01 01:10:02 -0500

    added more padding to footer on y-axis to push letters out of darkness of bg-gradient

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,8 +4,8 @@ import { BsGithub } from "react-icons/bs";
 const Footer = () => {
   return (
     <div className="">
-      <section className="bg-blue-500">
-        <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
+      <section className="bg-gradient-to-b from-black via-zinc-500 to-zinc-200">
+        <div className="max-w-screen-xl px-4 py-24 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
           <nav className="flex flex-wrap justify-center -mx-5 -my-2">
             <div className="px-5 py-2">
               <Link
