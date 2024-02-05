d4d23619ffd090494a3662bf4dfc49f05e883aba
Carlitos <carlitosdutan@pursuit.org>
2024-02-05 18:17:54 -0500

    fixed delete bug and added local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-//Packages
-import { Route, Routes } from "react-router-dom";
-
-// Components
+import { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Landing from "./Components/Landing";
 import Nav from "./Components/Nav";
 import MakeASpark from "./Components/CreateQuote";
-import FavoriteQuotes from "./Components/Favorite";
 import SavedSparks from "./Components/SavedQuotes";
 import Footer from "./Components/Footer";
 import { getAllQuotes, getAllBackgrounds } from "../data/fetch";
-import { useEffect, useState } from "react";
 import { AboutTheDevs } from "./Components/AboutTheDevs";
 
 function App() {
-  const [allQuotes, setAllQuotes] = useState([]); //state of Quotes
-  const [allBackgrounds, setAllBackgrounds] = useState([]); //state of Backgrounds
+  const [allQuotes, setAllQuotes] = useState([]);
+  const [allBackgrounds, setAllBackgrounds] = useState([]);
 
   useEffect(() => {
     getAllQuotes().then((quotes) => {
@@ -27,28 +23,24 @@ function App() {
   }, []);
 
   return (
-    <>
-    <div className="flex- flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen">
       <Nav />
       <Routes>
         <Route
-          path="src/SavedQuotes"
+          path="/src/SavedQuotes"
           element={<SavedSparks allQuotes={allQuotes} />}
         />
         <Route path="/CreateQuote" element={<MakeASpark />} />
-        <Route path="src/Devs" element={<AboutTheDevs />} />
+        <Route path="/AboutTheDevs" element={<AboutTheDevs />} />
         <Route
           path="/"
           element={
             <Landing allQuotes={allQuotes} allBackgrounds={allBackgrounds} />
           }
         />
-        <Route path="/aboutthedevs" element={<AboutTheDevs />} />
       </Routes>
-      
       <Footer />
-      </div>
-    </>
+    </div>
   );
 }
 
