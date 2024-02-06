9b18c80126162fd7162b212019729f4bb7a19632
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-02-06 11:46:45 -0500

    created a custom breakpoint for mobile devices with screen widths of 360 pixels

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "360",
+    },
     extend: {
       textShadow: {
-        default: '0 2px 4px rgba(0, 0, 0, 0.1)',
+        default: "0 2px 4px rgba(0, 0, 0, 0.1)",
       },
     },
   },
-  plugins: []
-}
+  plugins: [],
+};
